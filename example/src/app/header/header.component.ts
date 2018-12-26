8609b7222b5b69b72baaf25e1716a1a2bc810d11
Chris Lane <chris.lane359@gmail.com>
2018-12-26 11:15:02 -0800

    fixing build errors

--- example/src/app/header/header.component.ts
+++ example/src/app/header/header.component.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { RoutingSignalService } from '../routing-signal.service';
 
