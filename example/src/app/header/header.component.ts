3ae85eb9f63770403291cc2381dfc3e7e84e26d8
Chris Lane <chris.lane359@gmail.com>
2018-12-26 10:57:03 -0800

    adding header component

---
+++ example/src/app/header/header.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RoutingSignalService } from '../routing-signal.service';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+	constructor(private routingSignalService: RoutingSignalService) { }
+
+  @ViewChild('top') elementToFocusAfterRouting;
+
+         ngOnInit() {
+     this.routingSignalService.onRoute.subscribe( (e) => {
+                    this.onRoute(e);
+                 })
+   }
+
+         onRoute(e) {
+           console.log(e);
+                 //console.log(e.target);
+           if(e){
+                   this.elementToFocusAfterRouting.nativeElement.textContent = e.target.innerText;
+                 }
+           this.elementToFocusAfterRouting.nativeElement.focus();
+   }
+
+
+}
